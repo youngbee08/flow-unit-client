c80bb6e7f48994d10fb0933fe8945d2952091cbd
youngbee08 <youngbeeh08@gmail.com>
2025-12-16 13:50:32 +0100

    implemented PWA option, ignore .env

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,44 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "offline.html",
+        "dark_logo_circled.png",
+        "dark_logo_text-rbg.png",
+      ],
+      workbox: {
+        navigateFallback: "/offline.html",
+      },
+      manifest: {
+        name: "FlowUnit",
+        short_name: "FU",
+        start_url: "/dashboard/overview",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#757E8D",
+        icons: [
+          {
+            src: "/dark_logo_text-rbg.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/dark_logo_circled.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: { host: true },
 });
