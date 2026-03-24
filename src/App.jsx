b1993595822f0e02b1d841b23ce519e4e4aed14f
Bamitale Abdulazeem <youngbeeh08@gmail.com>
2026-03-24 15:25:04 +0100

    updated url issue

--- src/App.jsx
+++ src/App.jsx
@@ -21,158 +21,155 @@ import ResetPassword from "./pages/auth/ResetPassword";
 import NotFound from "./pages/NotFound";
 import TaskGenerator from "./pages/user/TaskGenerator";
 import TaskSmartGenerator from "./pages/user/TaskSmartGenerator";
-import AutoRedirect from "./components/AutoRedirect";
+// import AutoRedirect from "./components/AutoRedirect";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route element={<AutoRedirect />}>
-          <Route index element={<Login />} />
+        {/* <Route element={<AutoRedirect />}> */}
+        <Route index element={<Login />} />
 
-          <Route path="*" element={<NotFound />} />
-          <Route path="/create-account" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/create-account" element={<Signup />} />
 
-          <Route path="/verify-account" element={<VerifyEmail />} />
+        <Route path="/verify-account" element={<VerifyEmail />} />
 
-          <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
 
-          <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
 
-          <Route
-            path="/invitation/:teamId/"
-            element={
-              <ProtectedRoute>
-                <Invitation />
-              </ProtectedRoute>
-            }
-          />
+        <Route
+          path="/invitation/:teamId/"
+          element={
+            <ProtectedRoute>
+              <Invitation />
+            </ProtectedRoute>
+          }
+        />
 
-          <Route
-            path="/dashboard/overview"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<Overview />}
-                  pageName={"Overview"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/tasks"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<Tasks />}
-                  pageName={"Assigned Tasks"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/tasks/smart-generator"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<TaskSmartGenerator />}
-                  pageName={"AI Task Generator"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/projects"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<Projects />}
-                  pageName={"All Projects"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/projects/new"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<NewProject />}
-                  pageName={"Add new project"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/projects/:id"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<SingleProject />}
-                  pageName={"Project Details"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/projects/:id/addTask"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<TaskForm />}
-                  pageName={"New Task"}
-                  // pageInfo={
-                  //   "Use the form below to add a new task to your project. Provide clear and accurate details to help track progress, assign responsibility, and keep everything organized. Once added, this task will become part of your project workflow and can be updated or managed at any time."
-                  // }
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/projects/:id/ai/addTask"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<TaskGenerator />}
-                  pageName={"AI Task Generator"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/team"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<Teams />}
-                  pageName={"Team Management"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/notifications"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<Notifications />}
-                  pageName={"Notifications"}
-                />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/dashboard/profile-settings"
-            element={
-              <ProtectedRoute>
-                <DashboardsLayout
-                  children={<ProfileSettings />}
-                  pageName={"Account Settings"}
-                />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
+        <Route
+          path="/dashboard/overview"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout children={<Overview />} pageName={"Overview"} />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/tasks"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<Tasks />}
+                pageName={"Assigned Tasks"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/tasks/smart-generator"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<TaskSmartGenerator />}
+                pageName={"AI Task Generator"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/projects"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<Projects />}
+                pageName={"All Projects"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/projects/new"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<NewProject />}
+                pageName={"Add new project"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/projects/:id"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<SingleProject />}
+                pageName={"Project Details"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/projects/:id/addTask"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<TaskForm />}
+                pageName={"New Task"}
+                // pageInfo={
+                //   "Use the form below to add a new task to your project. Provide clear and accurate details to help track progress, assign responsibility, and keep everything organized. Once added, this task will become part of your project workflow and can be updated or managed at any time."
+                // }
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/projects/:id/ai/addTask"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<TaskGenerator />}
+                pageName={"AI Task Generator"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/team"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<Teams />}
+                pageName={"Team Management"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/notifications"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<Notifications />}
+                pageName={"Notifications"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/profile-settings"
+          element={
+            <ProtectedRoute>
+              <DashboardsLayout
+                children={<ProfileSettings />}
+                pageName={"Account Settings"}
+              />
+            </ProtectedRoute>
+          }
+        />
+        {/* </Route> */}
       </Routes>
       <Toaster expand closeButton visibleToasts={3} />
     </>
